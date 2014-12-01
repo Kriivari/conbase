84afe9a2384f9afd81f14a756b321992fa15f4bc
Joonas Iivonen <joonas.iivonen@iki.fi>
2014-12-01 22:28:48 +0200

    Fixing programgroups listing and forms

--- app/models/programgroup.rb
+++ app/models/programgroup.rb
@@ -2,5 +2,5 @@ class Programgroup < ActiveRecord::Base
   belongs_to :mailinglist
   has_and_belongs_to_many :programs, :join_table => 'programs_programgroups'
   cattr_reader :per_page
-  @@per_page = 10
+  @@per_page = 50
 end
