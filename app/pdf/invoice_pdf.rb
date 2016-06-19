0150bbfbc549a8f5a2377d28b5044f1d393ded27
Conbase Rails <conbase@kokoustamo.ropecon.fi>
2016-06-19 23:40:39 +0300

    Invoice tuning --Aketzu

--- app/pdf/invoice_pdf.rb
+++ app/pdf/invoice_pdf.rb
@@ -3,15 +3,15 @@ class InvoicePdf < Prawn::Document
     super()
     define_grid(:columns => 5, :rows => 8, :gutter => 0)
     grid(0,4).bounding_box() do
-      image "/usr/local/conbase/public/images/logo.png", :valign => :top, :position => :right, :width => 80
+      image "/srv/conbase/conbase/public/images/logo.png", :valign => :top, :position => :right, :width => 80
     end
     grid([0,0],[0,1]).bounding_box() do
       text "#{invoice.event.address}"
     end
     grid([1,0],[7,4]).bounding_box do
-      text "Kaubamaja-lasku", :position => :center, :size => 14, :style => :bold
+      text "Lasku", :position => :center, :size => 14, :style => :bold
       move_down 20
-      text "Laskutusosoite", :style => :bold
+      text "Asiakas", :style => :bold
       text "#{invoice.companyname}"
       text "#{invoice.billing_address}"
       move_down 20
@@ -27,23 +27,44 @@ class InvoicePdf < Prawn::Document
       rows = [ ["Tuote", "Kpl", "Yksikköhinta", "Alennus", "Yhteensä" ] ]
 
       if invoice.tables > 0
-	rows << ["Ensimmäinen myyntipöytä", "#{[ invoice.tables, 1 ].min()}", {:content => "#{"%.2f" % invoice.tableprice.to_s} €", :align => :right}, "#{invoice.rebate}%", {:content => "#{"%.2f" % ([ invoice.tables, 1 ].min() * invoice.tableprice * (100-invoice.rebate) / 100).to_s} €", :align => :right}]
+        #rows << ["Ensimmäinen myyntipöytä", "#{[ invoice.tables, 1 ].min()}", {:content => "#{"%.2f" % invoice.tableprice.to_s} €", :align => :right}, "#{invoice.rebate}%", {:content => "#{"%.2f" % ([ invoice.tables, 1 ].min() * invoice.tableprice * (100-invoice.rebate) / 100).to_s} €", :align => :right}]
+        rows << ["Myyntiosasto", invoice.tables, {:content => "%.2f €" % invoice.tableprice, :align => :right}, invoice.rebate.to_s + "%", {:content => "%.2f € " % (invoice.tables * invoice.tableprice * (100-invoice.rebate) / 100), :align => :right}]
       end
-      if invoice.tables > 1
-          rows << ["Seuraavat myyntipöydät", "#{[ invoice.tables - 1, 0 ].max()}", {:content => "#{"%.2f" % invoice.secondprice.to_s} €", :align => :right}, "#{invoice.rebate}%", {:content => "#{"%.2f" % ([ invoice.tables - 1, 0 ].max() * invoice.secondprice * (100-invoice.rebate) / 100).to_s} €", :align => :right}]
-      end
+      #if invoice.tables > 1
+      #    rows << ["Seuraavat myyntipöydät", "#{[ invoice.tables - 1, 0 ].max()}", {:content => "#{"%.2f" % invoice.secondprice.to_s} €", :align => :right}, "#{invoice.rebate}%", {:content => "#{"%.2f" % ([ invoice.tables - 1, 0 ].max() * invoice.secondprice * (100-invoice.rebate) / 100).to_s} €", :align => :right}]
+      #end
       if invoice.tickets > 0
           rows << ["Lisäranneke", "#{invoice.tickets}", {:content => "#{"%.2f" % invoice.ticketprice.to_s} €", :align => :right}, "#{invoice.rebate}%", {:content => "#{"%.2f" % (invoice.tickets * invoice.ticketprice * (100-invoice.rebate) / 100).to_s} €", :align => :right}]
       end
       if invoice.travelpasses > 0
-          rows << ["Kulkulupa", "#{invoice.travelpasses}", {:content => "0.00 €", :align => :right}, "#{invoice.rebate}%", {:content => "0,00 €", :align => :right}]
+          rows << ["Myyjäpassi", "#{invoice.travelpasses}", {:content => "0.00 €", :align => :right}, "#{invoice.rebate}%", {:content => "0,00 €", :align => :right}]
       end
 
+      prevname=""
+      prevprice=0
+      prevqty=0
+      
       for product_type in invoice.product_types
-        unless Exhibitor.special_type( product_type )
-          rows << [product_type.fullname, 1, {:content => "#{"%.2f" % product_type.price.to_s} €", :align => :right}, "#{invoice.rebate}%", {:content => "#{"%.2f" % (product_type.price * (100-invoice.rebate) / 100).to_s} €", :align => :right}]
-        end
+        next if Exhibitor.special_type( product_type )
+	if prevname != product_type.fullname || prevprice != product_type.price 
+	  if prevname != ""
+            rows << [prevname, prevqty, {:content => "%.2f €" % prevprice, :align => :right}, invoice.rebate.to_s + "%", {:content => "%.2f €" % (prevqty * prevprice * (100-invoice.rebate) / 100), :align => :right}]
+	  end
+	  prevname = product_type.fullname
+	  prevprice = product_type.price
+	  prevqty = 1
+
+	else
+	  prevqty += 1
+	end
+#        unless Exhibitor.special_type( product_type )
+#          rows << [product_type.fullname, 1, {:content => "#{"%.2f" % product_type.price.to_s} €", :align => :right}, "#{invoice.rebate}%", {:content => "#{"%.2f" % (product_type.price * (100-invoice.rebate) / 100).to_s} €", :align => :right}]
+#        end
       end
+      if prevname != ""
+        rows << [prevname, prevqty, {:content => "%.2f €" % prevprice, :align => :right}, invoice.rebate.to_s + "%", {:content => "%.2f €" % (prevqty * prevprice * (100-invoice.rebate) / 100), :align => :right}]
+      end
+
       rows << [{:content => "Laskun loppusumma:", :font_style => :bold},"","","",{:content => "#{"%.2f" % invoice.total.to_s} €", :align => :right, :font_style => :bold}]
       table( rows )
       move_down 20
